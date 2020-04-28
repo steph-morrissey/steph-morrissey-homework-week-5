9d1219e83f73baa6590aa92b43ef66b03d7df2bd
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-04-28 20:29:27 +0100

    Added rows and cols with relevant hours displayed

--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 $(document).ready(function () {
   console.log("ready!");
 });
-// Variable for displaying current date
-const currentDate = moment().format("dddd, MMMM Do");
-
-//Display current date
-function displayCurrentDate() {
-  $("#date-display").text(currentDate);
-}
-displayCurrentDate();
-// Declaring Array for timeblocks
 const timeblocksArray = [
   { id: "0", time: "09", meridiem: "am" },
   { id: "1", time: "10", meridiem: "am" },
@@ -22,3 +13,29 @@ const timeblocksArray = [
   { id: "7", time: "16", meridiem: "pm" },
   { id: "8", time: "17", meridiem: "pm" },
 ];
+// Variable for displaying current date
+const currentDate = moment().format("dddd, MMMM Do");
+
+//Display current date
+function displayCurrentDate() {
+  $("#date-display").text(currentDate);
+}
+
+// Display timeblocks within timeblocks div
+function hourlyTimeblock(hour, index) {
+  // create each timeblock row
+  const myRow = $("<div>").attr({ class: "row border-bottom" });
+  const hourCol = $("<div>")
+    .attr({ class: "col-2 p-2" })
+    .text(hour.time + hour.meridiem);
+  const eventsCol = $("<div>").attr({ class: "col-8 p-2" });
+  const deleteCol = $("<div>").attr({ class: "col-2 p-2" });
+  myRow.append(hourCol, eventsCol, deleteCol);
+  $(".eventsRow").append(myRow);
+}
+function displayTimeblocks() {
+  timeblocksArray.forEach(hourlyTimeblock);
+}
+displayCurrentDate();
+displayTimeblocks();
+// Declaring Array for timeblocks
