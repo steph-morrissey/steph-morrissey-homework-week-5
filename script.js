d34407b4d7567d8dc1f354ed68446623e2baeec7
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-04-28 19:37:09 +0100

    Declaring basic timeblocks array data

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
   console.log("ready!");
 });
 
-const now = moment();
-
-$("#date-display").text(now);
+// Declaring Array for timeblocks
+const timeblocksArray = [
+  { id: "0", time: "09", meridiem: "am" },
+  { id: "1", time: "10", meridiem: "am" },
+  { id: "2", time: "11", meridiem: "am" },
+  { id: "3", time: "12", meridiem: "pm" },
+  { id: "4", time: "13", meridiem: "pm" },
+  { id: "5", time: "14", meridiem: "pm" },
+  { id: "6", time: "15", meridiem: "pm" },
+  { id: "7", time: "16", meridiem: "pm" },
+  { id: "8", time: "17", meridiem: "pm" },
+];
